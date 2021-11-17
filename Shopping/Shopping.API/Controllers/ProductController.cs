22ee3f1f247cc83d16b0e51afac6eb06edcfc418
Cahit KAFADAR <cahit.kafadar@ardgrup.com.tr>
2021-11-17 18:12:26 +0300

    Dosya dizinleri duzeltildi

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Shopping.API.Data;
+
+namespace Shopping.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.Find(p=>true).ToListAsync();
+        }
+    }
+}
